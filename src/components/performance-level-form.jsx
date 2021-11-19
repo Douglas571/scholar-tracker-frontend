36fc67917aa53983686518b459b6f71476ad1d4e
Douglas Socorro <douglassocorro.work@gmail.com>
2021-11-18 21:41:59 -0400

    progress in the new performance level form

--- src/components/performance-level-form.jsx
+++ src/components/performance-level-form.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from 'react'
 
 export default function PerformanceLevelForm({ nextMinSLP, onSubmit }) {
 
+	let [level, setLevel] = useState(0)
+	let [ranges, setRanges] = useState([])
+
 	let [topSLP, setTopSLP] = useState(0)
 
 	let [scholarPercent, setScholarPercent] = useState(0)
@@ -11,7 +14,8 @@ export default function PerformanceLevelForm({ nextMinSLP, onSubmit }) {
 
 	useEffect(() => {
 		setTopSLP(nextMinSLP + 1)
-	}, [nextMinSLP])
+		console.log(`${JSON.stringify(ranges, null, 4)}`)
+	}, [nextMinSLP, ranges])
 
 	const handleInputChange = (evt) => {
 		console.log('PerformanceLevelForm - handleInputChange')
@@ -22,6 +26,9 @@ export default function PerformanceLevelForm({ nextMinSLP, onSubmit }) {
 		let newInvPercent = 0
 
 		switch(name) {
+			case 'level':
+				setLevel(value)
+				break
 
 			case 'top':
 				setTopSLP(value)
@@ -56,6 +63,20 @@ export default function PerformanceLevelForm({ nextMinSLP, onSubmit }) {
 		}
 	}
 
+	const handleNewRange = () => {
+		console.log(`PerformanceLevelForm - handle new range`)
+		const newRange = {
+			slp: topSLP,
+			percentage: {
+				scholar: scholarPercent,
+				managerPercent: managerPercent,
+				investor: invPercent
+			}
+		}
+
+		setRanges([...ranges, newRange])
+	}
+
 	const handleSubmit = (evt) => {
 		evt.preventDefault()
 		console.log('PerformanceLevelForm - handleSubmit')
@@ -80,36 +101,56 @@ export default function PerformanceLevelForm({ nextMinSLP, onSubmit }) {
 	return (
 		<div>
 			<h1>Escala de Desempeño</h1>
-	        <form onSubmit={ handleSubmit }>
-	            <p>
-	            	<label>SLP:</label>
-	            	<input type="number" name="bottom" 
-	            		value={ nextMinSLP } onChange={ handleInputChange } disabled/>
-	            	-
-	            	<input type="number" name="top"  value={ topSLP } onChange={ handleInputChange }
-	            	min={ nextMinSLP + 1 }/>
-	            </p>
-	            
-	            <h2>Porcentajes</h2>
-	            <p>
-	            	<label>Becado:</label>
-	            	<input type="number" name="scholar-percent"
-	            	min='0' max='100'
-	            	value={ scholarPercent } onChange={ handleInputChange }/>
-	            </p>
-	            <p>
-	            	<label>Manager:</label>
-	            	<input type="number" name="manager-percent"
-	            	min='0' max='100'
-	            	value={ managerPercent } onChange={ handleInputChange }/>
-	            </p>
-	            <p>
-	            	<label>Inversor:</label>
-	            	<input type="number" name="inv-percent" disabled
-	              		value={ invPercent } onChange={ handleInputChange }/>
-	            </p>
 
-	            <button type='submit'>Enviar</button>
+			<form onSubmit={ handleSubmit }>
+				<table border="1">
+					<thead>
+						<tr>
+							<th rowSpan="3">Nivel</th>
+							<th rowSpan="2">Rango de SLP</th>
+							<th colSpan="3">Porcentage de pagos</th>
+						</tr>
+						<tr>
+							<th>Becado</th>
+							<th>Manager</th>
+							<th>Inversor</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td rowSpan="2">
+								<input type="number" name="level" value={ level } onChange={ handleInputChange } required/>
+							</td>
+							<td>74</td>
+							<td>20%</td>
+							<td>30%</td>
+							<td>50%</td>
+							<td><button>x</button></td>
+						</tr>
+						<tr>
+							<td>
+								<input type="number" name="top"  value={ topSLP } onChange={ handleInputChange }
+		            				min={ nextMinSLP + 1 }/>
+		            		</td>
+							<td>
+								<input type="number" name="scholar-percent"
+					            	min='0' max='100'
+					            	value={ scholarPercent } onChange={ handleInputChange }/>
+							</td>
+							<td>
+								<input type="number" name="manager-percent"
+					            	min='0' max='100'
+					            	value={ managerPercent } onChange={ handleInputChange }/>
+							</td>
+							<td>
+								<input type="number" name="inv-percent" disabled
+		              				value={ invPercent } onChange={ handleInputChange }/>
+							</td>
+							<td onClick={ handleNewRange }><button>+</button></td>
+						</tr>
+					</tbody>
+				</table>
+				<button>Agregar Nivel</button>
 	        </form>
 		</div>
 	)
